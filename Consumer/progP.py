24e3704cea3e3b27227cf1d281ed3de685a118fa
Nicolas Simmonds Samper <n.simmonds10@uniandes.edu.co>
2018-03-25 23:31:57 -0500

    Update progP.py

--- Consumer/progP.py
+++ Consumer/progP.py
@@ -1,4 +1,6 @@
 import paho.mqtt.client as mqtt
+import time
+import requests
 
 def on_connect(client, userdata, flags, rc):
     if rc==0:
@@ -8,12 +10,20 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+    mail={
+          'body':userdata,
+          'sbj':'alarm'
+         }
+    url='http://172.23.65.161/correo'
+    correo={"From":+'servicio@yale.com'+"To: "+'residente@uniandes.edu.co'+"Subject: "+mail['sbj']+ "Body: "+'mail['body']}
+    req= requests.post(url,correo)
+            
+     
 
 
 client = mqtt.Client()
 client.subscribe("Apartamento1/residencia1/Alta/#")
-client.subscribe("Apartamento1/residencia1/Media/#")
 client.on_connect = on_connect
 client.on_message = on_message
-client.connect("172.23.65.161", 8083,60)
+client.connect("172.23.65.161", puerto=8083)
 client.loop_forever()
