86c3a56dfe7ce08de7e75ee54a352cd47bcde493
Nicolas <n.simmonds10@uniandes.edu.co>
2018-04-22 22:59:00 -0500

    agregado auth0 programa disparador

--- ProgramaDisparador/programaDisparador.py
+++ ProgramaDisparador/programaDisparador.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import threading, logging, time
 from kafka import KafkaConsumer, KafkaProducer
 import json
 from six.moves.urllib.request import urlopen
@@ -9,9 +8,105 @@ from flask import Flask, request, jsonify, _request_ctx_stack
 from flask_cors import cross_origin
 from jose import jwt
 
+AUTH0_DOMAIN = 'isis2503-jpotalora10.auth0.com'
+API_AUDIENCE = 'uniandes.edu.co/blocksecurity'
+ALGORITHMS = ["RS256"]
 
+app=Flask(__name__)
 producer = KafkaProducer(bootstrap_servers='localhost:8090')
 
+
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
+
+@APP.errorhandler(AuthError)
+def handle_auth_error(ex):
+    response = jsonify(ex.error)
+    response.status_code = ex.status_code
+    return response
+
+def get_token_auth_header():
+    """Obtains the Access Token from the Authorization Header
+    """
+    auth = request.headers.get("Authorization", None)
+    if not auth:
+        raise AuthError({"code": "authorization_header_missing",
+                         "description":
+                             "Authorization header is expected"}, 401)
+
+    parts = auth.split()
+
+    if parts[0].lower() != "bearer":
+        raise AuthError({"code": "invalid_header",
+                         "description":
+                             "Authorization header must start with"
+                             " Bearer"}, 401)
+    elif len(parts) == 1:
+        raise AuthError({"code": "invalid_header",
+                         "description": "Token not found"}, 401)
+    elif len(parts) > 2:
+        raise AuthError({"code": "invalid_header",
+                         "description":
+                             "Authorization header must be"
+                             " Bearer token"}, 401)
+
+    token = parts[1]
+    return token
+
+
+def requires_auth(f):
+    """Determines if the Access Token is valid
+    """
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = get_token_auth_header()
+        jsonurl = urlopen("https://"+AUTH0_DOMAIN+"/.well-known/jwks.json")
+        jwks = json.loads(jsonurl.read())
+        unverified_header = jwt.get_unverified_header(token)
+        rsa_key = {}
+        for key in jwks["keys"]:
+            if key["kid"] == unverified_header["kid"]:
+                rsa_key = {
+                    "kty": key["kty"],
+                    "kid": key["kid"],
+                    "use": key["use"],
+                    "n": key["n"],
+                    "e": key["e"]
+                }
+        if rsa_key:
+            try:
+                payload = jwt.decode(
+                    token,
+                    rsa_key,
+                    algorithms=ALGORITHMS,
+                    audience=API_AUDIENCE,
+                    issuer="https://"+AUTH0_DOMAIN+"/"
+                )
+            except jwt.ExpiredSignatureError:
+                raise AuthError({"code": "token_expired",
+                                 "description": "token is expired"}, 401)
+            except jwt.JWTClaimsError:
+                raise AuthError({"code": "invalid_claims",
+                                 "description":
+                                     "incorrect claims,"
+                                     "please check the audience and issuer"}, 401)
+            except Exception:
+                raise AuthError({"code": "invalid_header",
+                                 "description":
+                                     "Unable to parse authentication"
+                                     " token."}, 401)
+
+            _request_ctx_stack.top.current_user = payload
+            return f(*args, **kwargs)
+        raise AuthError({"code": "invalid_header",
+                         "description": "Unable to find appropriate key"}, 401)
+    return decorated
+
+@app.route("/claves/add")
+@cross_origin(headers=['Content-Type', 'Authorization'])
+@requires_auth
 def addPassword(val,index):
     if val<0 or val>9999:
         print("La clave debe contener como maximo 4 digitos")
@@ -20,6 +115,9 @@ def addPassword(val,index):
     else:
         producer.send('claves',"ADD_PASSWORD;"+str(val)+";"+str(index))
 
+@app.route("/claves/update")
+@cross_origin(headers=['Content-Type', 'Authorization'])
+@requires_auth
 def updatePassword(val,index):
     if int(val)<0 or int(val)>9999:
         print("La clave debe contener como maximo 4 digitos")
@@ -27,11 +125,19 @@ def updatePassword(val,index):
         print("El sistema solo puede almacenar 20 claves, y el indice debe ser mayor a 0")
     else:
         producer.send('claves',"UPDATE_PASSWORD;"+str(val)+";"+str(index))
+
+@app.route("/claves/delete1")
+@cross_origin(headers=['Content-Type', 'Authorization'])
+@requires_auth
 def deletePassword(index):
     if int(index)<1 or int(index)>20:
         print("El sistema solo puede almacenar 20 claves, y el indice debe ser mayor a 0")
     else:
         producer.send('claves',"DELETE_PASSWORD;"+str(index))
+
+@app.route("/claves/delete2")
+@cross_origin(headers=['Content-Type', 'Authorization'])
+@requires_auth
 def deleteAllPasswords():
     producer.send('claves',"DELETE_ALL")
 
@@ -46,4 +152,6 @@ elif len(parts)==2:
     dicc[parts[0]](parts[1])
 else:
     dicc[parts[0]]()
+
+
 
