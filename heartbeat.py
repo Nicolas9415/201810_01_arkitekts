5b583ab1884e13b36043d0dcc3e14bbf82f65026
Nicolas Simmonds Samper <n.simmonds10@uniandes.edu.co>
2018-05-09 00:43:14 -0500

    heartbeat

---
+++ heartbeat.py
@@ -0,0 +1,36 @@
+import paho.mqtt.client as paho
+from datetime import datetime
+import time
+import threading
+
+client = paho.Client("Py")
+client.connect("192.168.43.148", 1883)
+client.on_message=on_message
+
+status=""
+
+def on_message(client,usr,msg)
+    status=msg.payload
+
+topic = "residencia.checks"
+client.subsribe("topic")
+
+def send():
+    while True:
+        time.sleep(10)
+        client.publish(topic,"Todo bien")
+
+
+def recive():
+badChecks=0
+    while True:
+        if "Hub ok" in mensaje.payload():
+            client.publish("conjunto1/residencia1/heartbeathub", "todo bien")
+            badChecks = 0
+            time.sleep(10)
+
+        else:
+            badChecks=badchecks+1
+            time.sleep(10)
+        if badChecks >=5
+            client.publish("conjunto1/residencia1/propietario","CERRADURA FUERA DE LINEA")
